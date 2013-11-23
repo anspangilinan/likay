8a6752df68abd9de3c9f65528d22d2246dbdf57f
anspangilinan <anspangilinan@gmail.com>
2013-11-24 00:15:43 +0800

    adding initial usage of long/lat data to display cities

--- likay/views.py
+++ likay/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from core.models import Location
+
 
 def index(request, template="index.html"):
-    return render_to_response(template, {}, RequestContext(request))
+    context = {}
+    cities = Location.objects.all().order_by("name")
+
+    context = {
+        "cities": cities
+    }
+    
+    return render_to_response(template, context, RequestContext(request))
