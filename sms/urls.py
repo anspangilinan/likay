94a474c84d70ca0b93cd8b7351ada42631dd71b1
Mancio Basoc <mancio@ingenuity.ph>
2013-11-23 22:25:03 +0800

    Update on urls and view for message view

--- sms/urls.py
+++ sms/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('sms.views',
     url(r'^inbound/$', 'sms', name="fetch_sms"),
+	url(r'^message/$', 'post_message', name="post_message"),
 )
--- sms/views.py
+++ sms/views.py
@@ -6,13 +6,21 @@ from django.http import HttpResponse
 # Models
 from accounts.models import Subscriber
 from core.models import Location
+from sms.models import Message
 
 
 # KEYWORDS *note: value should be unicode
 KEYWORD = {
-    'subscribe': u'SUB',
+    'subscribe'  : u'SUB',
     'unsubscribe': u'UNSUB',
+    'message'    : u'MSG',
 }
+
+
+def get_text_content(data):
+    content = data['text'][0].split(' ')
+    return content
+
 
 def sms(request):
     """
@@ -22,16 +30,16 @@ def sms(request):
         data = {u'svc_id': [u'0'], u'rrn': [u'410063049575'], u'from': [u'639998419831'], u'text': [u'SUB DVO earvin'], u'smsc': [u'strikerS6800in1ngin'], u'to': [u'68002'], u'utime': [u'1385197906']}
 
         # Slice the text format: TYPE<space>CITY<space>NAME
-        content = data['text'][0].split(' ')
-
+        content = get_text_content(data)
         # Check what type of sms [SUB, UNSUB]
         if KEYWORD['subscribe'] == content[0]:
             print "subscribe"
             subscribe(data)
-
         elif KEYWORD['unsubscribe'] == content[0]:
             print "unsubscribe"
             unsubscribe(data)
+        elif KEYWORD['message'] == content[0]:
+            message(data)
         else:
             pass
             # Will send sms to user that their sms format is invalid
@@ -44,7 +52,7 @@ def subscribe(data):
     This function will check and save the user's data
     """
     # Slice the text format: TYPE<space>CITY<space>NAME
-    content = data['text'][0].split(' ')
+    content = get_text_content(data)
 
     # Get location
     location = Location.objects.get(code=str(content[1]))
@@ -52,8 +60,11 @@ def subscribe(data):
     # Check if the number already exist in the database
     subscriber, created = Subscriber.objects.get_or_create(phone=str(data['from'][0]))
     if created:
-        subscriber.name = content[2]
-
+    	# Save name (optional)
+    	try:
+        	subscriber.name = content[2]
+        except IndexError, e:
+        	pass
     subscriber.location.add(location)
     subscriber.save()
 
@@ -63,11 +74,33 @@ def unsubscribe(data):
     This function will check and unscubscribe the user's data
     """
     # Slice the text format: TYPE<space>CITY<space>NAME
-    content = data['text'][0].split(' ')
-
+    content = get_text_content(data)
     # Get location
     location = Location.objects.get(code=str(content[1]))
 
     subscriber = Subscriber.objects.get(phone=str(data['from'][0]))
     subscriber.location.remove(location)
     subscriber.save()
+
+
+def post_message(data):
+    """
+    This function will save the message and post on:
+    - ShoutOut Board
+    - Facebook
+    - Twitter
+    """
+    # Slice the text format: TYPE<space>CITY<space>NAME
+    content = split_content(data)
+    try:
+        message = content[1]
+        subscriber = Subscriber.objects.get(phone=str(data['from'][0]))
+    except IndexError, e:
+    	# Error: Send error message
+    	pass
+    else:
+    	# Save Message
+        message = Message.objects.create(content=message,
+										 subscriber=subscriber)
+    	message.save()
+    	# Add Facebook and twitter post here
