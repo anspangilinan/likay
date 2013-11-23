4c6b65b92fa760be7c73f3ef9348c5d4a7359b0b
Pat <pat@ingenuity.ph>
2013-11-23 22:03:55 +0800

    URL for receiving notifications during inbound sms changed to sms/inbound/; replaced tabs with spaces

--- sms/urls.py
+++ sms/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-
-
 urlpatterns = patterns('sms.views',
-	url(r'^fetching$', 'sms', name="fetch_sms"),
+    url(r'^inbound/$', 'sms', name="fetch_sms"),
 )
--- sms/views.py
+++ sms/views.py
@@ -10,64 +10,64 @@ from core.models import Location
 
 # KEYWORDS *note: value should be unicode
 KEYWORD = {
-	'subscribe': u'SUB',
-	'unsubscribe': u'UNSUB',
+    'subscribe': u'SUB',
+    'unsubscribe': u'UNSUB',
 }
 
 def sms(request):
-	"""
-	This view will catch the user's sms through a GET request from youphoric server.
-	"""
-	if request.method == "GET":
-		data = {u'svc_id': [u'0'], u'rrn': [u'410063049575'], u'from': [u'639998419831'], u'text': [u'SUB DVO earvin'], u'smsc': [u'strikerS6800in1ngin'], u'to': [u'68002'], u'utime': [u'1385197906']}
+    """
+    This view will catch the user's sms through a GET request from youphoric server.
+    """
+    if request.method == "GET":
+        data = {u'svc_id': [u'0'], u'rrn': [u'410063049575'], u'from': [u'639998419831'], u'text': [u'SUB DVO earvin'], u'smsc': [u'strikerS6800in1ngin'], u'to': [u'68002'], u'utime': [u'1385197906']}
 
-		# Slice the text format: TYPE<space>CITY<space>NAME
-		content = data['text'][0].split(' ')
+        # Slice the text format: TYPE<space>CITY<space>NAME
+        content = data['text'][0].split(' ')
 
-		# Check what type of sms [SUB, UNSUB]
-		if KEYWORD['subscribe'] == content[0]:
-			print "subscribe"
-			subscribe(data)
+        # Check what type of sms [SUB, UNSUB]
+        if KEYWORD['subscribe'] == content[0]:
+            print "subscribe"
+            subscribe(data)
 
-		elif KEYWORD['unsubscribe'] == content[0]:
-			print "unsubscribe"
-			unsubscribe(data)
-		else:
-			pass
-			# Will send sms to user that their sms format is invalid
+        elif KEYWORD['unsubscribe'] == content[0]:
+            print "unsubscribe"
+            unsubscribe(data)
+        else:
+            pass
+            # Will send sms to user that their sms format is invalid
 
-	return HttpResponse("awesomeness!")
+    return HttpResponse("awesomeness!")
 
 
 def subscribe(data):
-	"""
-	This function will check and save the user's data
-	"""
-	# Slice the text format: TYPE<space>CITY<space>NAME
-	content = data['text'][0].split(' ')
+    """
+    This function will check and save the user's data
+    """
+    # Slice the text format: TYPE<space>CITY<space>NAME
+    content = data['text'][0].split(' ')
 
-	# Get location
-	location = Location.objects.get(code=str(content[1]))
+    # Get location
+    location = Location.objects.get(code=str(content[1]))
 
-	# Check if the number already exist in the database
-	subscriber, created = Subscriber.objects.get_or_create(phone=str(data['from'][0]))
-	if created:
-		subscriber.name = content[2]
+    # Check if the number already exist in the database
+    subscriber, created = Subscriber.objects.get_or_create(phone=str(data['from'][0]))
+    if created:
+        subscriber.name = content[2]
 
-	subscriber.location.add(location)
-	subscriber.save()
+    subscriber.location.add(location)
+    subscriber.save()
 
 
 def unsubscribe(data):
-	"""
-	This function will check and unscubscribe the user's data
-	"""
-	# Slice the text format: TYPE<space>CITY<space>NAME
-	content = data['text'][0].split(' ')
+    """
+    This function will check and unscubscribe the user's data
+    """
+    # Slice the text format: TYPE<space>CITY<space>NAME
+    content = data['text'][0].split(' ')
 
-	# Get location
-	location = Location.objects.get(code=str(content[1]))
+    # Get location
+    location = Location.objects.get(code=str(content[1]))
 
-	subscriber = Subscriber.objects.get(phone=str(data['from'][0]))
-	subscriber.location.remove(location)
-	subscriber.save()
+    subscriber = Subscriber.objects.get(phone=str(data['from'][0]))
+    subscriber.location.remove(location)
+    subscriber.save()
