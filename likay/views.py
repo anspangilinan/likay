4c9aae6f289817aff4722fb96d36496956d1ccb2
anspangilinan <anspangilinan@gmail.com>
2013-11-23 21:53:08 +0800

    adding main page mockup

---
+++ likay/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index(request, template="index.html"):
+    return render_to_response(template, {}, RequestContext(request))
