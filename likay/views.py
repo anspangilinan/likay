3f2b1fea24e24091047526d2da59c1998fce2e6d
Johann <johann@Kyles-MacBook-Air.local>
2013-11-24 03:48:39 +0800

    added feed functionality

--- likay/views.py
+++ likay/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from core.models import Location
+from sms.models import Message
 
 
 def index(request, template="index.html"):
     context = {}
     cities = Location.objects.all().order_by("name")
 
+    # Feeds
+    messages = Message.objects.all().order_by('-date_received')
+
     context = {
-        "cities": cities
+        "cities": cities,
+        "messages": messages,
     }
     
     return render_to_response(template, context, RequestContext(request))
