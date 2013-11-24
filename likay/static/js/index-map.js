74aca80c1a18b4c42345bf6499c7dd07917b8be5
anspangilinan <anspangilinan@gmail.com>
2013-11-24 11:18:46 +0800

    adding map fixes

--- likay/static/js/index-map.js
+++ likay/static/js/index-map.js
@@ -1,11 +1,11 @@
 $(document).ready(function(event) {
-    var options =  {dragging: false, touchZoom: false, scrollWheelZoom: false, keyboard: false, zoomControl: false}
-        map = L.map('map', options).setView([11.6722, 122.9627], 5);
-    
-    L.tileLayer('http://{s}.tile.cloudmade.com/API-key/997/256/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-        maxZoom: 18
-    }).addTo(map);
+    var options = {dragging: false, touchZoom: false, scrollWheelZoom: false, keyboard: false, zoomControl: false}
+        map = L.map('map', options).setView([11.6722, 122.9627], 8),
+        cloudmadeUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg',
+        subDomains = ['1','2','3','4'],
+        cloudmade = new L.TileLayer(cloudmadeUrl, {subdomains: subDomains, maxZoom: 18});
+
+    map.addLayer(cloudmade);
 
     $("a[title='A JS library for interactive maps'").text("Likay");
 
@@ -14,8 +14,9 @@ $(document).ready(function(event) {
         var $selected = $(this).find(":selected"),
             latitude = $selected.data('latitude'),
             longitude = $selected.data('longitude'),
+            zoom = $selected.data('zoom'),
             weatherStatus = $selected.data('weather-status');
 
-        map.setView(latitude, longitude, 8.3);
+        map.setView([latitude, longitude], zoom);
     });
 });
